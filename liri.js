ae0ff8df533d463a4a0cf081db9f62e24a748f3b
Thorley <thorley.eric@gmail.com>
2018-07-30 19:28:25 -0400

    a bit cleaner

--- liri.js
+++ liri.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
-var Spotify = require('node-spotify-api')
-var fs = require('fs')
-var Twitter = require('twitter')
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var Twitter = require('twitter');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var searchType = process.argv[2]
+var searchType = process.argv[2];
 var searchQuery = process.argv[3];
 for (i = 4; i < process.argv.length; i++){
     searchQuery = searchQuery + "+" + (process.argv[i]) 
@@ -18,33 +18,33 @@ var tweetLookup = function(){
     var params = {screen_name: 'lirinodeproject'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (error){
-            console.log("error:" + error)
+            console.log("error:" + error);
         }
         if (!error) {
-            console.log("Liri node apps official tweets:")
+            console.log("Liri node apps official tweets:");
             for (i=0; i < tweets.length; i++){
-                console.log("\n" + tweets[i].text + "   | Tweeted on: " + tweets[i].created_at)
+                console.log("\n" + tweets[i].text + "   | Tweeted on: " + tweets[i].created_at);
             }
         }
     });
 }
 
 var movieLookup = function(){
-    console.log("Query:" + searchQuery)
-    if (searchQuery == undefined){searchQuery = "Mr.+Nodody"}
+    console.log("Query:" + searchQuery)//remove this
+    if (searchQuery === undefined){searchQuery = "Mr.+Nodody"}
     var queryUrl = "http://www.omdbapi.com/?t=" + searchQuery + "&y=&plot=short&apikey=aa361df7";
     request(queryUrl, function(error, response, body){
         if (!error && response.statusCode === 200) {
             console.log("Movie Title: " + JSON.parse(body).Title + "\nReleased: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating +
             "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nProduced in: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot +
             "\nStarring: " + JSON.parse(body).Actors);
-            fs.appendFile("random.txt", ","+ searchType + " " + searchQuery)
+            fs.appendFile("random.txt", ","+ searchType + " " + searchQuery);
         }
     })
 }
 
 var spotifyLookup = function(){
-    if (searchQuery === ""){searchQuery === "The+Sign"}
+    if (searchQuery === undefined){searchQuery === "The+Sign"}
     spotify.search({ type: 'track' , query: searchQuery, limit: 1})
         .then(function(data) {
         var information = data.tracks.items[0];
@@ -55,7 +55,7 @@ var spotifyLookup = function(){
         } 
         console.log("artist(s): " + artists);
         console.log ("Off of the album: " + information.album.name); 
-        console.log("Preview: " + information.preview_url)
+        console.log("Preview: " + information.preview_url);
         fs.appendFile("random.txt", ","+ searchType + " " + searchQuery)
         })
         .catch(function(err) {
@@ -66,34 +66,34 @@ var spotifyLookup = function(){
 var doIt = function(){
     fs.readFile("random.txt", 'utf8', function(err, data){
         if (err){
-            console.log("error: " + err)
+            console.log("error: " + err);
         }
         var terms = data.split(',')
-        var termsArray = [terms[0], terms[1]]
+        var termsArray = [terms[0], terms[1]];
             if (termsArray[0] === "movie-this"){
-                searchQuery = termsArray[1]
-                movieLookup()
+                searchQuery = termsArray[1];
+                movieLookup();
             }
             if (termsArray[0] === "spotify-this-song"){
-                searchQuery = termsArray[1]
-                spotifyLookup()
+                searchQuery = termsArray[1];
+                spotifyLookup();
            }
 
     })
 }
 
 if (searchType === "movie-this"){
-    movieLookup()
+    movieLookup();
 }
-if (searchType === "spotify-this-song"){
-     spotifyLookup()
-}
-
-if (searchType === 'my-tweets'){
-    tweetLookup()
+else if (searchType === "spotify-this-song"){
+     spotifyLookup();
 }
 
-if (searchType === "do-what-it-says"){
-    doIt()
+else if (searchType === 'my-tweets'){
+    tweetLookup();
+}
+
+else if (searchType === "do-what-it-says"){
+    doIt();
 }
 else {console.log("Commands are: movie-this , spotify-this-song , my-tweets & do-what-it-says")};
